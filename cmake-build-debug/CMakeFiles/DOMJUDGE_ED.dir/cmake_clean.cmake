ef1ab00e4664e0ba94aea593897e817412a2c3cd
airamsoto <airamxl9@gmail.com>
2024-05-23 16:14:41 +0200

    GOLA

---
+++ cmake-build-debug/CMakeFiles/DOMJUDGE_ED.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DOMJUDGE_ED.dir/x71.cpp.obj"
+  "CMakeFiles/DOMJUDGE_ED.dir/x71.cpp.obj.d"
+  "DOMJUDGE_ED.exe"
+  "DOMJUDGE_ED.exe.manifest"
+  "DOMJUDGE_ED.pdb"
+  "libDOMJUDGE_ED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DOMJUDGE_ED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- x71.cpp
+++ x71.cpp
@@ -1,3 +1,26 @@
+
+Se ha añadido el archivo adjunto x71.cpp.Ninguno seleccionado
+
+Ir al contenido
+        Uso de Gmail con lectores de pantalla
+Se ha marcado la conversación como spam.
+Más información  Deshacer
+        Conversaciones
+Instagram
+2
+New login on Instagram from Safari on Mac OS X
+- We've noticed a new login, airamsoto_ We've noticed a login from a device that you don't usually use. Mac OS X · Safari · Madrid, Spain April 22 at 1:48 PM (PDT) If this was you, you won
+
+22 abr
+        Colegio Mayor Nuest.
+SOLICITUD DE REINGRESO COLEGIO MAYOR GUADALUPE 2024-2025
+Archivo adjunto:
+REGLAMENTO_RÉGIMEN_INTERNO_GUADALUPE_2022-2023.pdf
+22 abr
+5,93 GB ocupados de 15 GB
+        Términos · Privacidad · Política del programa
+        Última actividad de la cuenta: hace 54 minutos
+        Detalles
 // Oficinas de empleo
 // ------------------
 // Estructuras de datos
@@ -19,54 +42,71 @@ using namespace std;
 
 class OficinaEmpleo {
 public:
-
     void altaOficina(const string &nombre, const string &empleo) {
-        if(!personaEmpleos[nombre].count(empleo)) {
+        if (!personaEmpleos[nombre].count(empleo)) {
             personaEmpleos[nombre].insert(empleo);
             empleosPersona[empleo].push_back(nombre);
+
+
+
+            empleoIterators[empleo][nombre] = --empleosPersona[empleo].end(); //todo revisar ejercicio 70 para ver si lo hice iugal
+
+
+
+
             personaEmpleosOrdenada[nombre].insert(empleo);
         }
-
     }
 
-    string ofertaEmpleo(string empleo) {
-        //TODO FALLA CASO altaOficina Ivan informatica
-        //altaOficina Jose informatica
-        //altaOficina Ivan administracion
-        //ofertaEmpleo administracion
-        //ofertaEmpleo informatica
-        //listadoEmpleos Ivan
-        //FIN
-        //TODO ahora fallan casos de prueba
-
+    string ofertaEmpleo(const string &empleo) {
         if (!empleosPersona.count(empleo)) {
-            throw std::domain_error("No existen personas apuntadas a este empleo");
+            throw domain_error("No existen personas apuntadas a este empleo");
         }
 
-        string devuelve = empleosPersona[empleo].back();
-        if(personaEmpleos.count(devuelve)) {
-            empleosPersona[empleo].pop_back();
-            if (empleosPersona[empleo].empty()) empleosPersona.erase(empleo);
-            personaEmpleos.erase(devuelve);
-            personaEmpleosOrdenada.erase(devuelve);
+        string persona = empleosPersona[empleo].front();
+        empleosPersona[empleo].pop_front();
 
-            return devuelve;
-        } else   throw std::domain_error("No existen personas apuntadas a este empleo");
 
+        empleoIterators[empleo].erase(persona);
+
+
+
+        if (empleosPersona[empleo].empty()) {
+            empleosPersona.erase(empleo);
+            empleoIterators.erase(empleo);
+        }
+
+        for (const auto &e : personaEmpleos[persona]) {
+            auto it = empleoIterators[e].find(persona);
+            if (it != empleoIterators[e].end()) {
+                empleosPersona[e].erase(it->second);
+                empleoIterators[e].erase(it);
+                if (empleosPersona[e].empty()) {
+                    empleosPersona.erase(e);
+                    empleoIterators.erase(e);
+                }
+            }
+        }
+
+        personaEmpleos.erase(persona);
+        personaEmpleosOrdenada.erase(persona);
+
+        return persona;
     }
 
-    vector<string> listadoEmpleos(string persona) {
-        if(!personaEmpleos.count(persona)) throw std::domain_error ("Persona inexistente");
-        return {personaEmpleosOrdenada[persona].begin(), personaEmpleosOrdenada[persona].end()};
+    vector<string> listadoEmpleos(const string &persona) const {
+        if (!personaEmpleos.count(persona)) {
+            throw domain_error("Persona inexistente");
+        }
+        return {personaEmpleosOrdenada.at(persona).begin(), personaEmpleosOrdenada.at(persona).end()};
     }
 
 private:
-    unordered_map<string, deque<string>> empleosPersona;
+    unordered_map<string, list<string>> empleosPersona;
+    unordered_map<string, unordered_map<string, list<string>::iterator>> empleoIterators;
     unordered_map<string, set<string>> personaEmpleosOrdenada;
-    unordered_map<string, set<string>> personaEmpleos;
+    unordered_map<string, unordered_set<string>> personaEmpleos;
 };
-
-
 
 bool tratar_caso() {
     OficinaEmpleo oe;
@@ -121,3 +161,5 @@ int main() {
     return 0;
 }
 
+x71.cpp
+        Mostrando x71.cpp.
